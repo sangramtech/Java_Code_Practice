995418583259667be81f7a353569312acff561a6
sangramtech <sangram.tech2015@gmail.com>
2022-04-04 21:51:53 -0500

    secondary branch added

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -6,6 +6,7 @@ public class GitIntegeration {
 		// TODO Auto-generated method stub
 		System.out.println("Git to GitHub");
 		System.out.println("Second commit push");
+		System.out.println("Secondary branch added");
 	}
 
 }
