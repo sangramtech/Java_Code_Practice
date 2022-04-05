f3e16b8a515c96b54f719cf4f8cb1b93b9935e45
sangramtech <sangram.tech2015@gmail.com>
2022-04-04 21:42:59 -0500

    test msg

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -5,7 +5,7 @@ public class GitIntegeration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git to GitHub");
-
+		System.out.println("Second commit push");
 	}
 
 }
