f6d8fe790e796240d31366d0f754ac1b57ee73cd
sangramtech <sangram.tech2015@gmail.com>
2022-04-20 15:04:58 -0500

    Changes for new branch

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -5,8 +5,7 @@ public class GitIntegeration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git to GitHub");
-		System.out.println("Second commit push");
-		System.out.println("Secondary branch added");
+		System.out.println("Added a new branch JavaLearning");
 	}
 
 }
