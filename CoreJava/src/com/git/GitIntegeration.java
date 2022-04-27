8f94c328efc40aa5fe6a7340bd2d3f158326b4dd
sangramtech <102498822+sangramtech@users.noreply.github.com>
2022-04-26 20:53:25 -0500

    Pull changes added

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -4,6 +4,7 @@ public class GitIntegeration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git fetch testing");		
+		System.out.println("Git fetch testing");
+		System.out.println("Git Pull testing");		
 	}
 }
