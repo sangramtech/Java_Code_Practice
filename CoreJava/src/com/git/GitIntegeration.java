ca98148dd32a216d83eabac1b826ba39d777e7ad
sangramtech <sangram.tech2015@gmail.com>
2022-04-26 20:50:53 -0500

    new changes for fetch

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -4,9 +4,6 @@ public class GitIntegeration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git to GitHub");
-		System.out.println("Added a new branch JavaLearning");
-		System.out.println("Merge completed");
+		System.out.println("Git fetch testing");		
 	}
-
 }
