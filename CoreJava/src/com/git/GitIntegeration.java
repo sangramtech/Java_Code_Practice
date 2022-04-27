32c80438b1f1025d6bbc790b18a2d069bc582747
sangramtech <sangram.tech2015@gmail.com>
2022-04-26 21:02:26 -0500

    merge testing

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -4,6 +4,9 @@ public class GitIntegeration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git fetch testing");		
+		System.out.println("Git fetch testing");
+		System.out.println("Git fetch without merge testing");
 	}
 }
+
+
