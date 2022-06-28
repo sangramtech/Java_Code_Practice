44727d163d9f1051cec3d71cae903e48c193c8e4
sangramtech <102498822+sangramtech@users.noreply.github.com>
2022-06-27 20:46:46 -0500

    Update GitIntegeration.java
    Committing changes.

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -5,6 +5,7 @@ public class GitIntegeration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GitHub commit");
+		System.out.println("Commited directly in GitHub");
 	}
 }
 
