010733c03c1102208a19abb7c040882c3f387d7d
sangramtech <sangram.tech2015@gmail.com>
2022-06-27 20:36:08 -0500

    github comment

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -4,10 +4,7 @@ public class GitIntegeration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git fetch testing");
-		System.out.println("Git fetch without merge testing");
-		System.out.println("Git Pull testing");		
-		System.out.println("Git fetch with merge");	
+		System.out.println("GitHub commit");
 	}
 }
 
