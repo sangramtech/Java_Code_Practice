9611b6f456fc69b9fdf60c4bb91db578f07fac88
sangramtech <sangram.tech2015@gmail.com>
2022-08-02 21:13:15 -0500

    update changes

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -3,11 +3,7 @@ package com.git;
 public class GitIntegeration {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("GitHub commit");
-		System.out.println("Commited directly in GitHub");
-		System.out.println("Changes from local repo");
-		System.out.println("git merge testing");
+
 	}
 }
 
