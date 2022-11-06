caf6f3052724e6499adb53a5165637b7eef07b79
sangramtech <sangram.tech2015@gmail.com>
2022-11-06 06:12:54 -0600

    remove test changes

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -5,7 +5,6 @@ public class GitIntegeration {
 	public static void main(String[] args) {
 		System.out.println("Added changes to git repo locally");
 		System.out.println("Added changes to github remote change");
-		System.out.println("Test branch changes");
 	}
 }
 
