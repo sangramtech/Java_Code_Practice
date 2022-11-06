b99fc4505872ca844adf27eca28f9092fd960156
sangramtech <102498822+sangramtech@users.noreply.github.com>
2022-11-06 05:55:20 -0600

    remote change

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -4,6 +4,7 @@ public class GitIntegeration {
 
 	public static void main(String[] args) {
 		System.out.println("Added changes to git repo locally");
+		System.out.println("Added changes to github remote change");
 	}
 }
 
