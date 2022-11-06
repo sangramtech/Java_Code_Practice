07f39413fc41ff3c8ce8ff7dba95c113cc64a864
sangramtech <sangram.tech2015@gmail.com>
2022-11-06 06:15:07 -0600

    Changes to test branch

--- CoreJava/src/com/git/GitIntegeration.java
+++ CoreJava/src/com/git/GitIntegeration.java
@@ -5,6 +5,7 @@ public class GitIntegeration {
 	public static void main(String[] args) {
 		System.out.println("Added changes to git repo locally");
 		System.out.println("Added changes to github remote change");
+		System.out.println("Added changes to test branch");
 	}
 }
 
